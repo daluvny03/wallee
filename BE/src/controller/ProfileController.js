2895512468e9eb89065823c2f8fd04378e8ccd72
daluvny03 <daluvny03@gmail.com>
2026-05-07 21:49:23 +0700

    update konsistensi status dan format respone pada profil dan auth

--- BE/src/controller/ProfileController.js
+++ BE/src/controller/ProfileController.js
@@ -3,18 +3,34 @@ import profileService from "../service/ProfileService.js";
 const getProfile = async (req, res) => {
   try {
     const profile = await profileService.getProfile(req.user.id);
-    res.status(200).json(profile);
+    res.status(200).json({
+      success: true,
+      message: "Data Profil sukses diambil",
+      data: profile
+    });
   } catch (err) {
-    res.status(404).json({ error: err.message });
+    res.status(404).json({
+      success: false,
+      message: err.message,
+      data: null
+    });
   }
 };
 
 const updateProfile = async (req, res) => {
   try {
     const updated = await profileService.updateProfile(req.user.id, req.body);
-    res.status(200).json(updated);
+    res.status(200).json({
+      success: true,
+      message: "Data Profil sukses diupdate",
+      data: updated
+    });
   } catch (err) {
-    res.status(404).json({ error: err.message });
+    res.status(404).json({
+      success: false,
+      message: err.message,
+      data: null
+    });
   }
 };
 
--- BE/src/controller/authController.js
+++ BE/src/controller/authController.js
@@ -3,18 +3,34 @@ import authService from "../service/authService.js";
 const register = async (req, res) => {
   try {
     const user = await authService.register(req.body);
-    res.status(200).json(user);
+    res.status(200).json({
+      success: true,
+      message: "Data Pengguna sukses dibuat",
+      data: user
+    });
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(400).json({ 
+      success: false,
+      message: err.message,
+      data: null
+     });
   }
 };
 
 const login = async (req, res) => {
   try {
     const data = await authService.login(req.body);
-    res.status(200).json(data);
+    res.status(200).json({
+      success: true,
+      message: "Login Berhasil",
+      data: data
+    });
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(400).json({ 
+      success: false,
+      message: err.message,
+      data: null
+     });
   }
 };
 
