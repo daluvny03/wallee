75b2692e82524729c2fa30b8f7935bcf9faf7810
daluvny03 <daluvny03@gmail.com>
2026-05-19 07:53:09 +0700

    setup route, auth, axios

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,33 +1,7 @@
-import Layout from "./components/layout/layout";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/dashboard";
-import Login from "./pages/login";
-import Register from "./pages/register";
-import Login2 from "./pages/login2";
-import Aichat from "./pages/chat";
-import Transaksi from "./pages/transaksi";
-import Analitik from "./pages/analitik";
-import Setting from "./pages/setting";
-import AddTransaction from "./pages/addTransaction";
+import AppRoutes from "./routes";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/login2" element={<Login2 />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Layout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/aichat" element={<Aichat />} />
-          <Route path="/transaksi" element={<Transaksi />} />
-          <Route path="/analytics" element={<Analitik />} />
-          <Route path="/settings" element={<Setting />} />
-          <Route path="/add-transaction" element={<AddTransaction />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
---
+++ FE/src/routes/index.jsx
@@ -0,0 +1,35 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "../pages/login";
+import Register from "../pages/register";
+import Dashboard from "../pages/dashboard";
+import Transaksi from "../pages/transaksi";
+import Chat from "../pages/chat";
+import Analitik from "../pages/analitik";
+import Setting from "../pages/setting";
+
+import Layout from "../components/layout/layout";
+import ProtectedRoute from "./protectedRoute";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Layout />
+          </ProtectedRoute>
+        }
+      >
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="transaksi" element={<Transaksi />} />
+        <Route path="chat" element={<Chat />} />
+        <Route path="analitik" element={<Analitik />} />
+        <Route path="setting" element={<Setting />} />
+      </Route>
+    </Routes>
+  );
+}
